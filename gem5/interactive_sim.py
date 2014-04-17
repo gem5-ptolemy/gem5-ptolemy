fd0ec8decfc617c89c6761c8daa3cda4969ad9e0
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2014-04-17 12:56:58 -0700

    set up for interactive sim

--- gem5/interactive_sim.py
+++ gem5/interactive_sim.py
@@ -3,27 +3,14 @@
 import os
 
 sim = "./build/ARM/gem5.opt"
-sim_opt = " --debug-flags=Bus,Cache"
+sim_opt = " --debug-start=1" + " --debug-flags=Bus,Cache"
 
-conf = " ./configs/example/se.py"
-max_tick = 10000
+conf = " ./configs/example/interactive.py"
+max_tick = 50000
 #conf_opt = " --checkpoint-at-end" + " --rel-max-tick=" + str(max_tick)
-conf_opt = " --checkpoint-at-end" + " --maxinsts=" + "10"
+conf_opt = " --rel-max-tick=" + str(max_tick)
 
 exe = " -c ../wcet_bench/matmult.o"
 
 os.system(sim + sim_opt + conf + conf_opt +  exe)
-
-restore_cnt = 1
-
-os.system(sim + sim_opt + conf + conf_opt + " -r " + str(restore_cnt) + exe)
-
-restore_cnt = 2
-
-os.system(sim + sim_opt + conf + conf_opt + " -r " + str(restore_cnt) + exe)
-
-restore_cnt = 3
-
-os.system(sim + sim_opt + conf + conf_opt + " -r " + str(restore_cnt) + exe)
-
 
