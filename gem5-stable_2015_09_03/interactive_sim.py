a8ffaa1fb78101b7129758db7b694200a701cf91
Hokeun Kim <hokeunkim@eecs.berkeley.edu>
2017-07-04 23:51:33 -0700

    fixed MiBench arguments and fixed Gem5Wrapper actor’s package name

--- gem5-stable_2015_09_03/interactive_sim.py
+++ gem5-stable_2015_09_03/interactive_sim.py
@@ -25,33 +25,38 @@ conf_opt += " --mem-type=DDR3_1600_x64"
 
 #exe = " -c ../wcet_bench/matmult.o"
 
+
 exe = " -c ../MiBench/consumer/jpeg/jpeg-6a/cjpeg"
 exe += " -o \"-dct int -progressive -opt -outfile ../MiBench/consumer/jpeg/output_large_encode.jpeg ../MiBench/consumer/jpeg/input_large.ppm\""
 
-#exe = " -c $MIBENCH_BIN/security/rijndael/rijndael"
-#exe += " -o \"$MIBENCH_BIN/security/rijndael/input_large.asc $MIBENCH_BIN/security/rijndael/output_large.enc \
+
+#exe = " -c ../MiBench/security/rijndael/rijndael"
+#exe += " -o \"../MiBench/security/rijndael/input_large.asc ../MiBench/security/rijndael/output_large.enc \
 # e 1234567890abcdeffedcba09876543211234567890abcdeffedcba0987654321\""
 
 
-#exe = " -c $MIBENCH_BIN/consumer/typeset/lout"
-#exe += " -o \"-I $MIBENCH_BIN/consumer/typeset/include -D $MIBENCH_BIN/consumer/typeset/data -F $MIBENCH_BIN/consumer/typeset/font \
-#                -C $MIBENCH_BIN/consumer/typeset/maps -H $MIBENCH_BIN/consumer/typeset/hyph $MIBENCH_BIN/consumer/typeset/small.lout\""
+#exe = " -c ../MiBench/consumer/typeset/lout-3.24/lout"
+#exe += " -o \"-I ../MiBench/consumer/typeset/lout-3.24/include -D ../MiBench/consumer/typeset/lout-3.24/data -F ../MiBench/consumer/typeset/lout-3.24/font \
+#                -C ../MiBench/consumer/typeset/lout-3.24/maps -H ../MiBench/consumer/typeset/lout-3.24/hyph ../MiBench/consumer/typeset/small.lout\""
 
-#exe = " -c $MIBENCH_BIN/network/dijkstra/dijkstra_large"
-#exe += " -o \"$MIBENCH_BIN/network/dijkstra/input.dat\""
 
-#exe = " -c $MIBENCH_BIN/network/patricia/patricia"
-#exe += " -o \"$MIBENCH_BIN/network/patricia/large.udp\""
+#exe = " -c ../MiBench/network/dijkstra/dijkstra_large"
+#exe += " -o \"../MiBench/network/dijkstra/input.dat\""
 
-#exe = " -c $MIBENCH_BIN/automotive/basicmath/basicmath_large"
 
-#exe = " -c $MIBENCH_BIN/consumer/jpeg/djpeg"
-#exe += " -o \"-dct int -ppm -outfile $MIBENCH_BIN/consumer/jpeg/output_small_decode.ppm $MIBENCH_BIN/consumer/jpeg/input_small.jpg\""
+#exe = " -c ../MiBench/network/patricia/patricia"
+#exe += " -o \"../MiBench/network/patricia/large.udp\""
+
+
+#exe = " -c ../MiBench/automotive/basicmath/basicmath_large"
+
+#exe = " -c ../MiBench/consumer/jpeg/djpeg"
+#exe += " -o \"-dct int -ppm -outfile ../MiBench/consumer/jpeg/output_small_decode.ppm ../MiBench/consumer/jpeg/input_small.jpg\""
                 
-#exe = " -c $MIBENCH_BIN/office/stringsearch/search_small"
+#exe = " -c ../MiBench/office/stringsearch/search_small"
 
-#exe = " -c $MIBENCH_BIN/automotive/susan/susan',
-#exe += " -o \"$MIBENCH_BIN/automotive/susan/input_large.pgm $MIBENCH_BIN/automotive/susan/output_large.smoothing.pgm -s\""
+#exe = " -c ../MiBench/automotive/susan/susan',
+#exe += " -o \"../MiBench/automotive/susan/input_large.pgm ../MiBench/automotive/susan/output_large.smoothing.pgm -s\""
 
 print 'starting the simulation!!!'
 
